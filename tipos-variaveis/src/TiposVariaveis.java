f0597c42b9c04b84b16b81f7f583e67a4ae653c0
Gabriel Dos Santos Beneti <santoslene9@gmail.com>
2024-08-06 17:15:06 -0300

    tipos e variaveis - 3

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,20 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
+        double SalarioMinimo = 2.500;
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21070333;
+        long cpf = 98765432109L;
+        float pi = 3.14F;
         System.out.println("Hello, World!");
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
     }
 }
+
+//tipos e Variaveis Java
+short numeroCurto = 1;
+int numeroNormal = numeroCurto;
+short numeroCurto2 = numeroNormal;
