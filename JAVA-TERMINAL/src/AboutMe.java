6d0c7d93dac364ef8ed5db1506ac027e2b167869
Gabriel Dos Santos Beneti <santoslene9@gmail.com>
2024-08-09 15:29:50 -0300

    sistema com terminais

---
+++ JAVA-TERMINAL/src/AboutMe.java
@@ -0,0 +1,29 @@
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AboutMe {
+    
+    public static void main (String[] args) {
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite eu nome");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade");
+        int idade = scanner.nextInt();
+
+        System.out.println("Sua altura");
+        double altura = scanner.nextDouble();
+
+        System.out.println("Olá, meu nome é " + nome + " " + sobrenome);
+        System.out.println("Minha idade é " + idade);
+        System.out.println("Tenho " + altura + " de altura");
+    }
+
+
+}
---
+++ JAVA-TERMINAL/src/Comentarios.java
@@ -0,0 +1,11 @@
+public class Comentarios {
+    public static void main(String[] args)  {
+       // Comentário de uma linha só
+
+       /* Um
+        * Comentario de 
+        * diversas 
+        * linhas
+        */
+    }
+}
